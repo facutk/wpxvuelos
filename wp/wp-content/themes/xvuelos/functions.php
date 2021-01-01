8dc43c18639a9f7a08ae974175c71e1f69e6eccf
facu.tk <facu.tk@gmail.com>
2020-12-31 21:38:58 -0300

    add initial userinfo

--- wp/wp-content/themes/xvuelos/functions.php
+++ wp/wp-content/themes/xvuelos/functions.php
@@ -47,7 +47,7 @@ if ( ! function_exists( 'xvuelos_frontend_scripts' ) ) {
   }
 }
 
-function hello_method(WP_REST_Request $request) {
+function get_foo_bar(WP_REST_Request $request) {
   $data = [ 'foo' => 'bar'];
 
   $response = new WP_REST_Response($data, 200);
@@ -56,12 +56,12 @@ function hello_method(WP_REST_Request $request) {
   return $response;
 }
 
-function ip_method(WP_REST_Request $request) {
+function get_ip(WP_REST_Request $request) {
   $ip  = $request->get_header('x-forwarded-for'); // heroku passes origin IP in this header
   if ($ip) {
     $ip = explode(',', $ip)[0];
   } else {
-    $ip = "186.139.211.36"; // @TODO move this to an env var
+    $ip = $_ENV["MOCK_IP_ADDRESS"];
   }
 
   $data = [ 'ip' => $ip ];
@@ -73,17 +73,91 @@ function ip_method(WP_REST_Request $request) {
   return $response;
 }
 
+function callAPI($method, $url, $data){
+  $curl = curl_init();
+  switch ($method){
+     case "POST":
+        curl_setopt($curl, CURLOPT_POST, 1);
+        if ($data)
+           curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        break;
+     case "PUT":
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+        if ($data)
+          curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        break;
+     default:
+        if ($data)
+           $url = sprintf("%s?%s", $url, http_build_query($data));
+  }
+  // OPTIONS:
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    //  'APIKEY: 111111111111111111111',
+     'Content-Type: application/json',
+  ));
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  // EXECUTE:
+  $result = curl_exec($curl);
+  if(!$result){die("Connection Failure");}
+  curl_close($curl);
+  return $result;
+}
+
+
+function get_userinfo(WP_REST_Request $request) {
+  $ip  = $request->get_header('x-forwarded-for'); // heroku passes origin IP in this header
+  if ($ip) {
+    $ip = explode(',', $ip)[0];
+  } else {
+    $ip = $_ENV["MOCK_IP_ADDRESS"];
+  }
+
+  $SKYSCANNER_URL = $_ENV["SKYSCANNER_URL"];
+  $SKYSCANNER_API_KEY = $_ENV["SKYSCANNER_API_KEY"];
+  // $url = "$SKYSCANNER_URL/autosuggest/v1.0/$country/$currency/$locale?query=$query&apiKey=$skyscanner_api_key";
+  $url = "$SKYSCANNER_URL/autosuggest/v1.0/US/USD/en?id=$ip-ip&apiKey=$SKYSCANNER_API_KEY";
+  $get_data = callAPI('GET', $url, false);
+  $response = json_decode($get_data, true);
+
+  $Places = $response['Places'];
+  $Place = $Places[0];
+  $market = explode('-', $Place["CountryId"])[0];
+  $CountryName = $Place["CountryName"];
+
+  $locale = 'es-MX';
+  $currency = 'USD';
+
+  $data = [
+    'locale' => $locale,
+    'market' => $market,
+    'currency' => $currency,
+    'country' => $CountryName
+  ];
+
+  $response = new WP_REST_Response($data, 200);
+  
+  $response->set_headers([ 'Cache-Control' => 'must-revalidate, no-cache, no-store, private' ]);
+  
+  return $response;
+}
+
 add_action( 'rest_api_init', function () {
   register_rest_route('xvuelos/v1', '/hello/', [
     'methods'  => WP_REST_Server::READABLE,
-    'callback' => 'hello_method',
+    'callback' => 'get_foo_bar',
   ]);
 
   register_rest_route('xvuelos/v1', '/ip/', [
     'methods'  => WP_REST_Server::READABLE,
-    'callback' => 'ip_method',
+    'callback' => 'get_ip',
+  ]);
+
+  register_rest_route('xvuelos/v1', '/userinfo/', [
+    'methods'  => WP_REST_Server::READABLE,
+    'callback' => 'get_userinfo',
   ]);
 });
-
 
 flush_rewrite_rules();
